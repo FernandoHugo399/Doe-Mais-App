956ca23748cb69c3cb324ebf1730eb83d06cc222
Hugo Fernando <huginho888@outlook.com>
2022-08-25 18:14:57 -0300

    Criando banner página home

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'doe-mais-App',
+  appName: 'Doe Mais',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
