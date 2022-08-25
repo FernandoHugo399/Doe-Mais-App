230fe8faa70f5e5aaf9448c0ef4d78f04a8fa365
Hugo Fernando <huginho888@outlook.com>
2022-08-25 14:13:45 -0300

    Configurando back button

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'doe-mais-App',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      androidScaleType: 'CENTER_CROP',
+      launchAutoHide: false
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { App } from '@capacitor/app';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { AlertController, Platform } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,64 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  private routesUrl = [];
+  constructor(
+    private platform: Platform,
+    private router: Router,
+    private alertController: AlertController,
+    private location: Location,
+  )
+  {
+    this.platform.ready().then(()=>{
+      SplashScreen.hide();
+      this.routerEvent();
+      this.backButtonEvent();
+    });
+  }
+
+  private routerEvent(){
+    this.router.events.pipe( filter( ( event: NavigationEnd ) => ( event instanceof NavigationEnd )))
+    .subscribe((event: NavigationEnd)=>{
+      if(this.routesUrl.length === 0) {
+        this.routesUrl.push(event.id, event.id);
+      } else {
+        this.routesUrl.push(event.id);
+      }
+    });
+  }
+
+  private backButtonEvent() {
+    this.platform.backButton.subscribeWithPriority(999999, ()=>{
+      const url = this.router.routerState.snapshot.url;
+      this.routesUrl.splice(-2);
+
+      if(url === '/tabs/home'){
+        if(this.routesUrl.length === 0) {
+          this.backButtonALert();
+        }
+      }
+
+      this.location.back();
+    });
+  }
+
+  private async backButtonALert() {
+    const alert = await this.alertController.create({
+      message: 'Você quer fechar o App ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'Cancel'
+        },
+        {
+          text: 'Fechar App',
+          handler: () => {
+            App.exitApp();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
