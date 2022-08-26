b2813e6182cdf767bca8b28c4c68f912d7732d81
Hugo Fernando <huginho888@outlook.com>
2022-08-25 21:46:34 -0300

    Finalizando home v1

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component, OnInit } from '@angular/core';
 import { Institutions } from 'src/app/services/institutions/institutions.model';
 import { InstitutionsService } from 'src/app/services/institutions/institutions.service';
 
@@ -7,10 +8,56 @@ import { InstitutionsService } from 'src/app/services/institutions/institutions.
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
   public institutions!: Institutions;
-  constructor(private institutionsService: InstitutionsService) {
+  public option_slide = {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      280:{
+        slidesPerView: 2.5,
+        spaceBetween: 8
+      },
+      340:{
+        slidesPerView: 3.5,
+        spaceBetween: 10
+      },
+      440: {
+        slidesPerView: 4.5,
+        spaceBetween: 14
+      },
+      660: {
+        slidesPerView: 5.5,
+        spaceBetween: 18
+      },
+      900: {
+        slidesPerView: 6.5,
+        spaceBetween: 25
+      },
+      1280: {
+        slidesPerView: 7.5,
+        spaceBetween: 30
+      },
+      1600: {
+        slidesPerView: 8.5,
+        spaceBetween: 35
+      }
+    }
+  };
 
+  constructor(private institutionsService: InstitutionsService) { }
+
+
+
+  ngOnInit(): void {
+    this.getAllInstitutions();
   }
 
+  getAllInstitutions(): void {
+    this.institutionsService.getAllInstitutions()
+    .subscribe( res =>  this.institutions = res );
+  }
 }
